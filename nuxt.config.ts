6a17971787db3e73aef7f6e570cc3a4cb4066cb6
Mohamed Slimani <slimani.m1993@gmail.com>
2025-06-04 23:53:22 +0100

    Add baseURL support and update font paths dynamically
    Introduced support for a configurable `baseURL` to handle dynamic asset paths based on the environment. Updated references to assets (e.g., favicon, PWA icons) and removed redundant LCDDot font-face declaration in `tailwind.css`. This ensures better flexibility and consistency across deployments.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 const appName = 'Clock ScreenSaver';
 const appShortName = 'Clock';
 const description = 'A customizable digital clock screen saver with multiple fonts, styles, and display options. Features various digital clock faces and transitions for a modern, retro, or minimalist look.';
+const baseURL = process.env.NUXT_APP_BASE_URL || '/'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   app: {
@@ -19,7 +21,7 @@ export default defineNuxtConfig({
         lang: 'en',
       },
       link: [
-        {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+        {rel: 'icon', type: 'image/x-icon', href: `${baseURL}/favicon.ico`},
       ]
     }
   },
@@ -32,7 +34,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
-    "@vite-pwa/nuxt"
+    '@vite-pwa/nuxt'
   ],
   css: ['~/assets/css/tailwind.css'],
   vite: {
@@ -57,17 +59,17 @@ export default defineNuxtConfig({
       theme_color: '#ffffff',
       icons: [
         {
-          src: '/pwa-192x192.png',
+          src: `${baseURL}/pwa-192x192.png`,
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: '/pwa-512x512.png',
+          src: `${baseURL}/pwa-512x512.png`,
           sizes: '512x512',
           type: 'image/png',
         },
         {
-          src: '/pwa-512x512.png',
+          src: `${baseURL}/pwa-512x512.png`,
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any maskable',
@@ -82,29 +84,30 @@ export default defineNuxtConfig({
       disableLocalFallbacks: true
     },
     families: [
-      { name: 'Share Tech Mono', provider: 'google' },
-      { name: 'VT323', provider: 'google' },
-      { name: 'Orbitron', provider: 'google' },
-      { name: 'Nova Mono', provider: 'google' },
-      { name: 'Source Code Pro', provider: 'google' },
-      { name: 'UnifontEX', provider: 'fontsource' },
-      { name: 'Workbench', provider: 'fontsource' },
-      { name: 'Kola', provider: 'fontshare' },
-      { name: 'Styro', provider: 'fontshare' },
-      { name: 'Array', provider: 'fontshare' },
-      { name: 'Technor', provider: 'fontshare' },
-      { name: 'Nippo', provider: 'fontshare' },
-      { name: "Calculator", src: "/fonts/Calculator.ttf" },
-      { name: "Chuck Chillout", src: "/fonts/ChuckChillout.ttf" },
-      { name: "delia Black", src: "/fonts/delibl__.ttf" },
-      { name: "delia", src: "/fonts/delirg__.ttf" },
-      { name: "DISPLAY TFB", src: "/fonts/DISPLAYFREETFB.ttf" },
-      { name: "Energized", src: "/fonts/Energized.ttf" },
-      { name: "HydrogenType", src: "/fonts/hydrogen.ttf" },
-      { name: "JD DigiSquare", src: "/fonts/jd_digisquare.ttf" },
-      { name: "LED Calculator", src: "/fonts/LEDCalculator.ttf" },
-      { name: "LED Dot-Matrix", src: "/fonts/LEDDot-Matrix.ttf" },
-      { name: "Seven Segment", src: "/fonts/SevenSegment.ttf" },
+      {name: 'Share Tech Mono', provider: 'google'},
+      {name: 'VT323', provider: 'google'},
+      {name: 'Orbitron', provider: 'google'},
+      {name: 'Nova Mono', provider: 'google'},
+      {name: 'Source Code Pro', provider: 'google'},
+      {name: 'UnifontEX', provider: 'fontsource'},
+      {name: 'Workbench', provider: 'fontsource'},
+      {name: 'Kola', provider: 'fontshare'},
+      {name: 'Styro', provider: 'fontshare'},
+      {name: 'Array', provider: 'fontshare'},
+      {name: 'Technor', provider: 'fontshare'},
+      {name: 'Nippo', provider: 'fontshare'},
+      {name: 'Calculator', src: '/fonts/Calculator.ttf'},
+      {name: 'Chuck Chillout', src: '/fonts/ChuckChillout.ttf'},
+      {name: 'delia Black', src: '/fonts/delibl__.ttf'},
+      {name: 'delia', src: '/fonts/delirg__.ttf'},
+      {name: 'DISPLAY TFB', src: '/fonts/DISPLAYFREETFB.ttf'},
+      {name: 'Energized', src: '/fonts/Energized.ttf'},
+      {name: 'HydrogenType', src: '/fonts/hydrogen.ttf'},
+      {name: 'JD DigiSquare', src: '/fonts/jd_digisquare.ttf'},
+      {name: 'LED Calculator', src: '/fonts/LEDCalculator.ttf'},
+      {name: 'LED Dot-Matrix', src: '/fonts/LEDDot-Matrix.ttf'},
+      {name: 'Seven Segment', src: '/fonts/SevenSegment.ttf'},
+      {name: 'LCDDot TR', src: '/fonts/lcddot_tr.woff2'},
     ]
   },
 })
